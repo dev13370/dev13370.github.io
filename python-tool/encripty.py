44fd420967c162206658eb0b15942914964b5e27
dev13370 <89332611+dev13370@users.noreply.github.com>
2021-08-22 13:48:41 +0300

    Delete python-tool directory

--- python-tool/encripty.py
+++
@@ -1,5 +0,0 @@
-ip = "154.16.115.34"
-port = 80
-#data = {"mifunsusmxckvucncuxkxx"}
-for send in range(50):
-    print(send, ip, port)
--- python-tool/scanner.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-req = requests.get('https://webscantest.com/')
-
-print(req.headers)
